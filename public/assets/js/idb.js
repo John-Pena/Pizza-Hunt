1834c5234ea69c4e32cbd2744e4d7a6f45617a90
John-Pena <jmp1495@gmail.com>
2022-06-03 11:24:37 -0500

    stored user created pizza in local storage if offline and tested

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -27,3 +27,15 @@ request.onerror = function(event) {
   // log error here
   console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(['new pizza'], 'readwrite');
+
+  // access the object store for `new pizza`
+  const pizzaObjectStore = transaction.objectStore('new pizza');
+
+  // add record to your store with add method
+  pizzaObjectStore.add(record);
+};
